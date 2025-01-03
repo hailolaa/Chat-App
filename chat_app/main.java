8cf765c525ee434cc57d0195b4db09278b8fda90
Hailemichael Molla <167626795+hailolaa@users.noreply.github.com>
2025-01-03 05:31:30 +0300

    Add files via upload

---
+++ chat_app/main.java
@@ -0,0 +1,12 @@
+package com.example.chat_app;
+
+public class main {
+    public static void main(String[] args) {
+        for (int i = 10; i > 0; i--) {
+            System.out.println(i + 2);
+            i -= 4;
+
+        }
+    }
+}
+
